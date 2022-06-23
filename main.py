f4e733fd86993e0e8a72465d620ee0702f0822dc
hisham-vaporio <hisham@vapor.io>
2022-06-23 19:12:03 -0400

    fix foo, add http request

--- main.py
+++ main.py
@@ -3,8 +3,17 @@
 def foo(a):  # NonCompliant
     b = 12
     if a == 1:
-        return b
+        return b-1
     return b
+
+from django.http import HttpResponse
+
+def index(request):
+    value = request.GET.get("value")
+    response = HttpResponse("")
+    response["Set-Cookie"] = value  # Noncompliant
+    response.set_cookie("sessionid", value)  # Noncompliant
+    return response
 
 print("Hello World!")
 print("Does this work?")
